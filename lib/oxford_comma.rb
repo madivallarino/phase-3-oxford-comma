2a6475a9c864d844a856b9da25b9619552aa9d23
Madison <madivallarino@gmail.com>
2021-09-21 09:33:14 -0400

    commit tree be0a12e053f5c13559aff98d647e17bce36dfb80

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+   new_array = array.join(",")
+   if array.length == 1 
+    array.join
+   elsif array.length == 2
+    array.join(' and ')
+   else 
+    new_arr = array.pop
+   answer = array.join(',') << "and #{new_arr}"
 end
+    return answer
+end
+
+binding.pry
+
+# if the list is greater than 2, add an ,
+# and on the last index, add an and 
